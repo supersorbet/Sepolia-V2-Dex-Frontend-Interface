fa451499258fd6f010ea6eed562734ff8e86fe9f
supersorbet <artificialperiod@gmail.com>
2023-09-01 01:45:47 -0400

    ititle

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -67,7 +67,7 @@ function MyApp(props: AppProps<{ initialReduxState: any }>) {
         />
         <meta
           name="description"
-          content="Cheaper and faster than Uniswap?"
+          content="Cheap?"
         />
         <meta name="theme-color" content="#1FC7D4" />
         <meta name="twitter:image" content="" />
@@ -76,8 +76,8 @@ function MyApp(props: AppProps<{ initialReduxState: any }>) {
           content="st."
         />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="🥞 PepeDex - A next evolution DeFi exchange on BNB Smart Chain (BSC)" />
-        <title>PepeDex</title>
+        <meta name="twitter:title" content="🐸 PepeDex V2 - The Frenliest DEX in DeFi." />
+        <title>PepeDex V2</title>
         {(Component as NextPageWithLayout).mp && (
           // eslint-disable-next-line @next/next/no-sync-scripts
           <script src="https://public.bnbstatic.com/static/js/mp-webview-sdk/webview-v1.0.0.min.js" id="mp-webview" />
